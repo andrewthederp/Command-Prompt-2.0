5715b36eb096306b9312c1b8163682457d517801
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2023-04-23 01:10:49 +0200

    bumped version to `1.0.6`

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ desc = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="command_prompt",
-    version="1.0.5",
+    version="1.0.6",
     description="A copy of command prompt that allows you to easily make cli apps",
     long_description=desc,
     long_description_content_type="text/markdown",
