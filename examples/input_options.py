a37e192892203b03d7e16e99bb51533a5487d995
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2023-05-30 15:11:22 +0300

    Update input_options.py

--- examples/input_options.py
+++ examples/input_options.py
@@ -1,37 +1,49 @@
-from command_prompt import Window
-import pygame
+from command_prompt import Window, TextInput, CursorShape
 
 window = Window()
 
 def input_options(options, *, prefix='', selected_option_prefix=''):
 
-    current_option = 0
-    y, _ = window.cursor_pos
+	def inp_func(key, user_text, text_input, *, y):
+		current_option = 0 if type(user_text) == str else user_text
+		if key['unicode'] == '\r':
+			text_input.getting_input = False
+		elif key['key'] == 1073741906:
+			current_option = (current_option - 1) % len(options)
+		elif key['key'] == 1073741905:
+			current_option = (current_option + 1) % len(options)
 
-    while window.running:
-        for i, option in enumerate(options):
-            window.move_cursor_to(0, y+i)
-            if i == current_option:
-                window.print(selected_option_prefix+option+'\n')
-            else:
-                window.print(prefix+option+'\n')
+		for i, option in enumerate(options):
+			window.move_cursor_to(0, y+i)
+			if i == current_option:
+				window.print(selected_option_prefix+option+'\n')
+			else:
+				window.print(prefix+option+'\n')
+		return current_option
 
-        first_press = window.get_pressed_key()
-        if window.current_button and (first_press or pygame.time.get_ticks() >= window.current_button['next_press']):
-            if window.current_button['unicode'] == '\r':
-                break
-            elif window.current_button['key'] == 1073741906:
-                current_option = (current_option - 1) % len(options)
-            elif window.current_button['key'] == 1073741905:
-                current_option = (current_option + 1) % len(options)
+	y, _ = window.cursor_pos
 
-    return current_option
+	for i, option in enumerate(options):
+		window.move_cursor_to(0, y+i)
+		if i == 0:
+			window.print(selected_option_prefix+option+'\n')
+		else:
+			window.print(prefix+option+'\n')
+
+	text_input = TextInput(inp_func, func_args={'y':y})
+
+	window.change_cursor(CursorShape.INVISIBLE)
+	while text_input.getting_input and window.running:
+		pass
+
+	window.change_cursor(CursorShape.BAR)
+	return text_input.user_text
 
 
 def main():
-    animals = ['cat', 'dog', 'duck', 'rat', 'horse']
-    window.print("What is your favourite animal:\n")
-    option = input_options(animals, prefix='[ ] ', selected_option_prefix='[*] ')
-    window.print('\n'+"Wow, my favourite animal is "+animals[option]+' too!')
+	animals = ['cat', 'dog', 'duck', 'rat', 'horse']
+	window.print("What is your favourite animal:\n")
+	option = input_options(animals, prefix='[ ] ', selected_option_prefix='[*] ')
+	window.print('\n'+"Wow, my favourite animal is "+animals[option]+' too!')
 
 window.run(main)
